0888c5764e6bd1f26c003d202a43c53b8836788d
palakjain21 <21palak2001@gmail.com>
2021-01-06 00:37:18 +0530

    added sidebar with navbar and profile

--- src/components/map.js
+++ src/components/map.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 import { MapApiKey } from '../keys';
 import mapStyles from './mapStyles';
-import Navbar from '../components/navbar/Navbar';
 
+import Sidebar from './Sidebar/Sidebar';
 const options = {
   styles: mapStyles,
 };
@@ -21,118 +21,19 @@ function map() {
 
 const WrappedMap = withScriptjs(withGoogleMap(map));
 const RenderMap = () => {
-  const [isNav, setIsNav] = useState(false);
-
-  const toggleNav = () => {
-    setIsNav(!isNav);
-  };
-  console.log(isNav);
   return (
-    <>
-      <Navbar isNav={isNav} />
-      {isNav ? (
-        <div
-          style={{
-            height: '100vh',
-            width: '100vw',
-            position: 'fixed',
-            top: 0,
-            zIndex: 0,
-          }}
-        >
-          <WrappedMap
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${MapApiKey}&v=3.exp&libraries=geometry,drawing,places`}
-            loadingElement={<div style={{ height: '100%' }} />}
-            containerElement={<div style={{ height: '100%' }} />}
-            mapElement={<div style={{ height: '100%' }} />}
-          />
-          <button
-            style={{
-              position: 'fixed',
-              zIndex: '10',
-              top: '10rem',
-              left: '18rem',
-            }}
-            onClick={toggleNav}
-          >
-            <svg
-              version="1.1"
-              id="Capa_1"
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              height="30px"
-              width="30px"
-              viewBox="0 0 384 384"
-              style={{ enableBackground: 'new 0 0 384 384' }}
-              xmlSpace="preserve"
-            >
-              <g>
-                <g>
-                  <g>
-                    <rect x="0" y="277.333" width="384" height="42.667" />
-                    <rect x="0" y="170.667" width="384" height="42.667" />
-                    <rect x="0" y="64" width="384" height="42.667" />
-                  </g>
-                </g>
-              </g>
-            </svg>
-          </button>
-        </div>
-      ) : (
-        <div
-          style={{
-            height: '100vh',
-            width: '100vw',
-            position: 'fixed',
-            top: 0,
-            zIndex: '0',
-          }}
-        >
-          <WrappedMap
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${MapApiKey}&v=3.exp&libraries=geometry,drawing,places`}
-            loadingElement={<div style={{ height: '100%' }} />}
-            containerElement={<div style={{ height: '100%' }} />}
-            mapElement={<div style={{ height: '100%' }} />}
-          />
-          <button
-            style={{
-              position: 'fixed',
-              zIndex: '10',
-              top: '10rem',
-              left: '1rem',
-            }}
-            onClick={toggleNav}
-          >
-            <svg
-              version="1.1"
-              id="Capa_1"
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              height="30px"
-              width="30px"
-              viewBox="0 0 384 384"
-              style={{
-                enableBackground: 'new 0 0 384 384',
-                backgroundColor: 'white',
-              }}
-              xmlSpace="preserve"
-            >
-              <g>
-                <g>
-                  <g>
-                    <rect x="0" y="277.333" width="384" height="42.667" />
-                    <rect x="0" y="170.667" width="384" height="42.667" />
-                    <rect x="0" y="64" width="384" height="42.667" />
-                  </g>
-                </g>
-              </g>
-            </svg>
-          </button>
-        </div>
-      )}
-    </>
+    <div>
+      <Sidebar />
+
+      <div style={{ height: '100vh', width: '100vw', zIndex: '0' }}>
+        <WrappedMap
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${MapApiKey}&v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{ height: '100%' }} />}
+          containerElement={<div style={{ height: '100%' }} />}
+          mapElement={<div style={{ height: '100%' }} />}
+        />
+      </div>
+    </div>
   );
 };
 export default RenderMap;
