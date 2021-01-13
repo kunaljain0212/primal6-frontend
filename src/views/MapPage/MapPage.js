0a974c1ccdfe20e3542e348a16f3d17a43abf625
Kunal Jain <jaikunal209@gmail.com>
2021-01-14 01:20:05 +0530

    Fix: New map and folder structur

---
+++ src/views/MapPage/MapPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from '../../components/map/Map';
+import Sidebar from '../../components/Sidebar/Sidebar';
+
+const RenderMap = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Map />
+    </div>
+  );
+};
+export default RenderMap;
